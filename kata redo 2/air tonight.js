edd434a22953fdbc3554ceabc3a04e39e963dd81
Daniel Suen <danielsuen200791357@gmail.com>
2023-11-24 15:09:59 -0500

    finished kata 7

---
+++ kata redo 2/air tonight.js
@@ -0,0 +1,20 @@
+const checkAir = function (samples, threshold) {
+  let clean = samples.filter(element => element==="clean").length;
+  let percent = (samples.length - clean)/samples.length
+  return percent >= threshold ? "polluted" : "clean"
+};
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
