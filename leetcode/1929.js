43c135069327c5eac24123fffd721ca39cb18378
Daniel Suen <danielsuen2007@yahoo.ca>
2023-12-01 12:33:10 -0500

    synced folders

---
+++ leetcode/1929.js
@@ -0,0 +1,10 @@
+var getConcatenation = function(nums) {
+  let ans = [];
+  for (let i = 0; i < nums.length; i++) {
+      ans.push(nums[i])
+  } 
+  for (let i = 0; i < nums.length; i++) {
+      ans.push(nums[i])
+  } 
+  return ans
+};
